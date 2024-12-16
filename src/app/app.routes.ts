2f5978102b29405f54d28906ac4a26570b86bbff
Kaneki <00szczepanskibartosz00@gmail.com>
2024-12-16 18:35:31 +0100

    wqe3we

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'about',component:MainpageComponent}
+];
